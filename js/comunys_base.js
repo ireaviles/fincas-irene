0e13c961f9cd5bcb96453a898c2a4ebb73d9ffad
Irene <ireneavilestrabajo@gmail.com>
2017-05-31 12:38:49 +0200

    }

--- js/comunys_base.js
+++ js/comunys_base.js
@@ -739,7 +739,7 @@ function getSaldoH(cargaPagos) {
 }
 
 
-function ajustar_pago_page( ui ){}
+function ajustar_pago_page( ui ){
 
 
 	chkLocalNames();
@@ -1651,5 +1651,3 @@ if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g)) {
 }
 
 }
-
-
